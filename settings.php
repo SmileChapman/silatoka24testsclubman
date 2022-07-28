818395ccd2554306def10d97e4446d68ed5c4ebf
Sergio <56945130+Sergio-ClubMan@users.noreply.github.com>
2022-07-28 10:58:24 +0300

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php if( !defined('FW') ) die('Silence is golden');
+
+$options = array(
+	'logo-image' => array(
+		'type' => 'upload',
+		'label' => __('logo', 'silatoka24wp'),
+		'images-only' => true
+	)
+);
