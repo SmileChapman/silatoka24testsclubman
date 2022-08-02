752901b9c46320afa4cc6734796754e9635a701c
SmileChapman <90320689+SmileChapman@users.noreply.github.com>
2022-08-02 10:45:34 +0300

    changes_1
    filesfromehomework

--- settings.php
+++
@@ -1,9 +0,0 @@
-<?php if( !defined('FW') ) die('Silence is golden');
-
-$options = array(
-	'logo-image' => array(
-		'type' => 'upload',
-		'label' => __('logo', 'silatoka24wp'),
-		'images-only' => true
-	)
-);
